9efe97c9b747cad2c76e900772b6042a0b4c66fe
Toby McLaughlin <toby@jarpy.net>
2017-02-16 10:57:23 +1100

    Sligtly better test names

--- test/common/test_user.py
+++ test/common/test_user.py
@@ -1,13 +1,13 @@
 from ..fixtures import beat
 
 
-def test_group(Group, beat):
+def test_group_properties(Group, beat):
     group = Group(beat.name)
     assert group.exists
     assert group.gid == 1000
 
 
-def test_user(User, beat):
+def test_user_properties(User, beat):
     user = User(beat.name)
     assert user.uid == 1000
     assert user.gid == 1000
