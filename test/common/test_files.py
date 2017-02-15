4b726e16b9bf39c3087427b67e58be02bba3fa0d
Toby McLaughlin <toby@jarpy.net>
2017-02-15 16:29:20 +1100

    Test Beats CLI flags

--- test/common/test_files.py
+++ test/common/test_files.py
@@ -3,14 +3,14 @@ import os
 beat = os.environ['BEAT']
 
 
-def test_binary_file(File):
+def test_binary_file_permissions(File):
     binary = File("/usr/share/%s/%s" % (beat, beat))
     assert binary.user == beat
     assert binary.group == beat
     assert binary.mode == 0o0755
 
 
-def test_config_file(File):
+def test_config_file_permissions(File):
     config = File("/usr/share/%s/%s.yml" % (beat, beat))
     assert config.user == beat
     assert config.group == beat
--- test/common/test_process.py
+++ test/common/test_process.py
@@ -3,18 +3,27 @@ import pytest
 
 beat = os.environ['BEAT']
 
+
 @pytest.fixture()
-def beat_process(Process):
+def process(Process):
     return Process.get(comm=beat)
 
 
-def test_beat_process(beat_process):
-    assert beat_process.pid == 1
+def test_process_is_pid_1(process):
+    assert process.pid == 1
 
 
-def test_process_is_running_as_the_correct_user(beat_process):
+def test_process_is_running_as_the_correct_user(process):
     if beat == 'packetbeat':
         correct_user = 'root'
     else:
         correct_user = beat
-    assert beat_process.user == correct_user
+    assert process.user == correct_user
+
+
+def test_process_was_started_with_the_verbose_flag(process):
+    assert '-v' in process['args']
+
+
+def test_process_was_started_with_the_foreground_flag(process):
+    assert '-e' in process['args']
