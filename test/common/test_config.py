2f54e4d0543a1fbf232595aabca8f8504427396d
Toby McLaughlin <toby@jarpy.net>
2017-02-17 11:21:59 +1100

    Run configtest in test suite

---
+++ test/common/test_config.py
@@ -0,0 +1,6 @@
+from ..fixtures import beat
+
+
+def test_config_file_passes_config_test(Command, beat):
+    configtest = '%s -c %s -configtest' % (beat.binary_file.path, beat.config_file.path)
+    Command.run_expect([0], configtest)
