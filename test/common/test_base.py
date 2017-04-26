e4fd1a79664cf3fdc81bef0362f38167609dfd80
Toby McLaughlin <toby@jarpy.net>
2017-04-26 16:02:48 +1000

    Use centos:7 as base

--- test/common/test_base.py
+++ test/common/test_base.py
@@ -1,3 +1,3 @@
 def test_base_os(SystemInfo):
-    assert SystemInfo.distribution == 'ubuntu'
-    assert SystemInfo.release == '16.04'
+    assert SystemInfo.distribution == 'centos'
+    assert SystemInfo.release == '7'
