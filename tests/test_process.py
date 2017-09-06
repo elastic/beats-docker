4529e9977470b91191afe457f9b07ac63cbd5bf3
Toby McLaughlin <toby@jarpy.net>
2017-09-06 06:03:01 +0200

    Test the Auditbeat runs as root

--- tests/test_process.py
+++ tests/test_process.py
@@ -6,7 +6,10 @@ def test_process_is_pid_1(beat):
 
 
 def test_process_is_running_as_the_correct_user(beat):
-    assert beat.process.user == beat.name
+    if beat.name == 'auditbeat':
+        assert beat.process.user == 'root'
+    else:
+        assert beat.process.user == beat.name
 
 
 def test_process_was_started_with_the_foreground_flag(beat):
